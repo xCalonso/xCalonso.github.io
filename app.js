f974b3b3dd6ddb7760a75605564060a78eda5f5e
xCalonso <96618051+xCalonso@users.noreply.github.com>
2023-08-08 16:15:40 +0200

    Formulario

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 const value = document.querySelector("#value")
 const input = document.querySelector("#input")
+const select = document.querySelector("#medicamentos")
+const formula = document.querySelector("#formula")
+const solucion = document.querySelector("#solucion")
 
-value.textContent = input.value
+const medicamentos = {anectine: "1mg·kg",
+                      atropina: "",
+                      dexametasona: "",
+                      fentanilo: "",
+                      lidocaina: "1mg·kg",
+                      ondansetron: "",
+                      propofol: "",
+                      rocuronio: "",
+                      ibuprofeno: "",
+                      enantyum: "",
+                      nolotil: "40mg·kg",
+                      paracetamol: ""
+                    }
+const formulas = {anectine: 1,
+                  atropina: "",
+                  dexametasona: "",
+                  fentanilo: "",
+                  lidocaina: 1,
+                  ondansetron: "",
+                  propofol: "",
+                  rocuronio: "",
+                  ibuprofeno: "",
+                  enantyum: "",
+                  nolotil: 40,
+                  paracetamol: ""
+                }
+
+value.value = input.value
 input.addEventListener("input", (event) => {
-  value.textContent = event.target.value
+  value.value = event.target.value
+  calcularMedicina()
 })
+
+value.addEventListener("keyup", (event) => {
+  if (event.key == "Enter"){
+    calcularMedicina()
+  }
+})
+
+formula.textContent = medicamentos[select.value]
+select.addEventListener("change", (event) => {
+  formula.textContent = medicamentos[event.target.value]
+  calcularMedicina()
+})
+
+function calcularMedicina(){
+  var aux = formulas[select.value] * value.value
+  solucion.textContent = aux
+}
